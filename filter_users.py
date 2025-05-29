77effef4a37d5345fa2a6f05c32de1974f2927db
Omid444 <86495623+Omid444@users.noreply.github.com>
2025-05-29 08:20:20 +0200

    PEP8 improved, Bugs fixed

--- filter_users.py
+++ filter_users.py
@@ -1,54 +1,72 @@
 import json
 
 
-def filter_users_by_name(name):
+USERS_FILE = "users.json"
+
+
+def read_file():
+    """Getting all users data at once"""
+    with open(USERS_FILE, "r") as file:
+        users = json.load(file)
+        return users
+
+
+def filter_users_by_name(name, users_file):
     """Filter users data base on name and print them"""
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    filtered_users = [user for user in users_file if user["name"].lower() == name.lower()]
+    if filtered_users:
+        for user in filtered_users:
+            print(user)
+    else:
+        print("***User not found in database***")
 
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
-    for user in filtered_users:
-        print(user)
-
-def filter_users_by_age(age):
+def filter_users_by_age(age, users_file):
     """Filter users data based on age and print them"""
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    filtered_users = [user for user in users_file if user["age"] == age]
 
-    filtered_users = [user for user in users if user["age"] == age]
+    if filtered_users:
+        for user in filtered_users:
+            print(user)
+    else:
+        print("***User not found in database***")
 
-    for user in filtered_users:
-        print(user)
 
-
-def filter_users_by_email(email):
+def filter_users_by_email(email, users_file):
     """Filter users data based on email address and print them"""
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    filtered_users = [user for user in users_file if user["email"].lower() == email.lower()]
 
-    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+    if filtered_users:
+        for user in filtered_users:
+            print(user)
+    else:
+        print("***User not found in database***")
 
-    for user in filtered_users:
-        print(user)
+
+def main():
+    users = read_file()  # loading all user data
+
+    while True:
+        filter_option = input("What would you like to filter by? ( name, age or email): ").strip().lower()
+
+        if filter_option == "name":
+            name_to_search = input("Enter a name to filter users:").strip()
+            filter_users_by_name(name_to_search, users)
+
+        elif filter_option == "age":
+            try:
+                age_to_search = int(input("Enter an age to filter users:").strip())
+                filter_users_by_age(age_to_search, users)
+            except ValueError:
+                print("Please enter a valid number for age.")
+
+        elif filter_option == "email":
+            email_to_search = input("Enter a email to filter users:").strip()
+            filter_users_by_email(email_to_search, users)
+
+        else:
+            print("***Filtering by that option is not yet supported***")
+
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? ( 'name','age' or 'email'): ").strip().lower()
-
-    if filter_option == "name":
-        name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
-
-    elif filter_option == "age":
-        try:
-            age_to_search = int(input("Enter an age to filter users: ").strip())
-            filter_users_by_age(age_to_search)
-        except ValueError:
-            print("Please enter a valid number for age.")
-
-    elif filter_option == "email":
-        email_to_search = input("Enter a email to filter users: ").strip()
-        filter_users_by_email(email_to_search)
-
-    else:
-        print("Filtering by that option is not yet supported.")
+    main()
